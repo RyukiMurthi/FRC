982b636cb5ef8029728362b5199fc32b571c5e2e
RyukiMurthi <99290844+RyukiMurthi@users.noreply.github.com>
2022-04-08 12:59:21 +1200

    Add files via upload
    Number checker

---
+++ 00_FRC_base_v1.py
@@ -0,0 +1,33 @@
+# import libraries
+
+
+# *** Functions go here ***
+
+# checks that input is either a float or an
+# integer that is more than zero. Takes in custom error message
+def num_check (question, error, num_type):
+    valid = False
+    while not valid:
+
+        try:
+            response = num_type(input(question))
+
+            if response <= 0:
+                print (error)
+            else:
+                return response
+
+        except ValueError:
+            print (error)
+
+
+# main routine goes here
+get_int = num_check("How many do you need?: ",
+                    "Please enetr an amount more than 0\n",
+                    int)
+get_cost = num_check ("How much does it cost? $",
+                      "Please enter a nummber more than 0\n",
+                      float)
+
+print ("You need: {}".format(get_int))
+print ("It costs: ${}".format (get_cost))
